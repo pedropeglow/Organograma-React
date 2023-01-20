67d94e955ff196c04600c551355cd3cc693172bc
pedropeglow <pedropeglowm@gmail.com>
2023-01-20 16:54:25 -0300

    :sparkles: Atualizando projeto.

---
+++ src/banner/Banner.js
@@ -0,0 +1,10 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="./images/banner.png" alt="O banner principal da página" />;
+    </header>
+  );
+}
+export default Banner;
