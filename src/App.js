709b19cc1c1c5be2e38a7215fa645991fb1e31af
pedropeglow <pedropeglowm@gmail.com>
2023-02-27 17:24:02 -0300

    :sparkles: Adicionando lista de posições.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import Formulario from "./components/Formulario";
 import Time from "./components/Time";
 
 function App() {
+  const posicoes = [
+    {
+      nome: "Goleiro",
+      corPrimaria: "#57c278",
+      corSecundaria: "#d9f7e9",
+    },
+    {
+      nome: "Defensor",
+      corPrimaria: "#02CFFA",
+      corSecundaria: "#e8f8ff",
+    },
+    {
+      nome: "Meio-Campo",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#f0f8e2",
+    },
+    {
+      nome: "Ataque",
+      corPrimaria: "#E06869",
+      corSecundaria: "#FDE7E8",
+    },
+  ];
+
   const [jogadores, setJogadores] = useState([]);
 
   const aoNovoJogadorAdicionado = (jogador) => {
