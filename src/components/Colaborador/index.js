2b1e9818e9d20626b91cc72a995c0c51a3e4c57e
pedropeglow <pedropeglowm@gmail.com>
2023-03-01 17:28:56 -0300

    :sparkles: Desenvolvimento de card.

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/pedropeglow.png" alt></img>
+      </div>
+      <div className="rodape">
+        <h4>Pedro Peglow</h4>
+        <h5>Desenvolvedor</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
