bbe6e853fb5d4bb4aff9946349520d3b75055c0a
pedropeglow <pedropeglowm@gmail.com>
2023-03-30 17:42:09 -0300

    :lipstick: Ajustando css.

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,7 @@ import "./Banner.css";
 function Banner() {
   return (
     <header className="banner">
-      <img src="./images/banner.png" alt="O banner principal da página" />;
+      <img src="./images/banner.png" alt="O banner principal da página" />
     </header>
   );
 }
