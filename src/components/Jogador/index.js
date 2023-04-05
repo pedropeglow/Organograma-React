b62854009c48d2b6a383f75fa9ab5799e195090d
pedropeglow <pedropeglowm@gmail.com>
2023-04-05 15:59:24 -0300

    :lipstick: Adicionando icone.

--- src/components/Jogador/index.js
+++ src/components/Jogador/index.js
@@ -1,9 +1,15 @@
 import "./Jogador.css";
+import { AiFillCloseCircle } from "react-icons/ai";
 
-const Jogador = ({ nome, imagem, posicao, corDeFundo }) => {
+const Jogador = ({ nome, imagem, posicao, corDeFundo, aoDeletar }) => {
   return (
     <div className="jogador">
-      <div className="cabecalho" style={{backgroundColor: corDeFundo}}>
+      <AiFillCloseCircle
+        size={25}
+        className="deletar"
+        onClick={aoDeletar}
+      ></AiFillCloseCircle>
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
         <img src={imagem} alt="Imagem de jogador"></img>
       </div>
       <div className="rodape">
